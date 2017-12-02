cfbd09008818fd8d6cc7d2976aded9394a23d243
[TheIncredibleK] <keith.kny@gmail.com>
2017-12-02 17:24:52 +0000

    this shit doesn't even fucking work you cunt

---
+++ Game Jam/Assets/sripts/Request/TestRequest.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class TestRequest : MonoBehaviour
+{
+
+    private static string apiLink = "http://ec2-54-201-19-112.us-west-2.compute.amazonaws.com:8000/api/";
+    public string _playerId;
+    public string _currentGameId;
+    public string _mapId;
+    void Start()
+    {
+        StartCoroutine(_createPlayerId("kennyk"));
+    }
+
+    IEnumerator GetText()
+    {
+        using (UnityWebRequest request = UnityWebRequest.Get("http://unity3d.com/"))
+        {
+            yield return request.SendWebRequest();
+
+            if (request.isNetworkError) // Error
+            {
+                Debug.Log(request.error);
+            }
+            else // Success
+            {
+                Debug.Log(request.downloadHandler.text);
+            }
+        }
+    }
+    public void CreatePlayerId(string username) {
+        StartCoroutine(_createPlayerId(username));
+    }
+    IEnumerator _createPlayerId(string username) {
+        var postData = new List<IMultipartFormSection>();
+        postData.Add(new MultipartFormFileSection("username", username));
+        Debug.Log(postData);
+        using (UnityWebRequest request = UnityWebRequest.Post(apiLink, postData)) {
+            yield return request.SendWebRequest();
+            if (request.isNetworkError) // Error
+            {
+                Debug.Log(request.error);
+            }
+            else // Success
+            {
+                _playerId = request.downloadHandler.text;
+                Debug.Log(_playerId);
+            }
+
+        }
+    }
+}
