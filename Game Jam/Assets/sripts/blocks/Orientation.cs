26ae0cfe4b907887e0ed931acad06ba8c14eb99c
[TheIncredibleK] <keith.kny@gmail.com>
2017-12-02 15:38:55 +0000

    lots of shit

---
+++ Game Jam/Assets/sripts/blocks/Orientation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Orientation {
+    Undefined,
+    Up, 
+    Right,
+    Down,
+    Left,
+    Wombles
+}
+
+
