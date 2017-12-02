628ae3af4cb8d5e76cc50e2ef2035a03d1c302e5
peter f <peterf@recommenderx.com>
2017-12-02 16:28:27 +0000

    rayCasting

---
+++ Game Jam/Assets/sripts/Map/moveCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveCamera : MonoBehaviour {
+
+    public GameObject mapBuilder;
+	// Use this for initialization
+	void Start () {
+        MapBuilder mapScript = mapBuilder.GetComponent<MapBuilder>();
+        Vector3 topLeft = mapBuilder.transform.position;
+        Vector2 size = new Vector2(mapScript.sizeX * mapScript.tilesize, mapScript.sizeZ * mapScript.tilesize);
+        Vector3 Centre = new Vector3(topLeft.x + (size.x/2), mapBuilder.transform.position.y, topLeft.z + (size.y/2));
+        this.transform.position = new Vector3(Centre.x, Centre.y+50, Centre.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Game Jam/Assets/sripts/ui/PlayerState.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerState : MonoBehaviour {
+    private bool itemSelected = false;
+    public GameObject trash;
+
+    public float maxObjects;
+    public float curObjects;
+
+    public GameObject spaceBar;
+    private SpaceBar barScript;
+
+    public Camera camera;
+
+    // Use this for initialization
+    void Start()
+    {
+        trash.SetActive(itemSelected);
+        barScript = spaceBar.GetComponentInChildren<SpaceBar>();
+        barScript.setMaxObjects(maxObjects);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Transform objectHit = hit.transform;
+                GameObject objectl = hit.transform.gameObject;
+                Debug.Log(objectl.transform.position);
+                // Do something with the object that was hit by the raycast.
+            }
+        }
+    }
+
+    public void flipItemSelected()
+    {
+        itemSelected = !itemSelected;
+        trash.SetActive(itemSelected);
+    }
+
+    public void placeObject()
+    {
+        curObjects += 1;
+        barScript.setCurObjects(curObjects);
+    }
+
+}
