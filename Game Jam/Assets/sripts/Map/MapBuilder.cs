a46e74c721b3c8267ae6ad073bf329eb95a18f37
[jamalm] <[bluedocjamal@hotmail.co.uk]>
2017-12-02 11:51:25 +0000

    added placeholders

---
+++ Game Jam/Assets/sripts/Map/MapBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapBuilder : MonoBehaviour {
+
+    public int sizeX = 0;
+    public int sizey = 0;
+
+    public List<GridSlot> slots;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
